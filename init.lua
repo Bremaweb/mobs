00cb8fc595468ce10918498ba35dc5aca675355c
Brandon <brandon@bremaweb.com>
2014-04-29 09:37:55 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,854 @@
+dofile(minetest.get_modpath("mobs").."/api.lua")
+
+-- NPCs and Barbarians
+mobs.npcs = {
+				[0] = "mobs:female1_npc", 
+				[1] = "mobs:female2_npc", 
+				[2] = "mobs:female3_npc", 
+				[3] = "mobs:male1_npc", 
+				[4] = "mobs:male2_npc",
+				[5] = "mobs:male3_npc",
+			}
+mobs.barbarians = {
+				[0] = "mobs:barbarian1", 
+				[1] = "mobs:barbarian2"
+				}
+
+mobs:register_mob("mobs:dirt_monster", {
+	type = "monster",
+	hp_min = 3,
+	hp_max = 27,
+	exp_min = 0,
+	exp_max = 35,
+	collisionbox = {-0.4, -0.01, -0.4, 0.4, 1.9, 0.4},
+	visual = "mesh",
+	mesh = "mobs_stone_monster.x",
+	textures = {"mobs_dirt_monster.png"},
+	visual_size = {x=3, y=2.6},
+	makes_footstep_sound = true,
+	view_range = 15,
+	walk_velocity = 1.25,
+	run_velocity = 3.75,
+	damage = 2,
+	drops = {
+		{name = "default:dirt",
+		chance = 1,
+		min = 3,
+		max = 5,},
+	},
+	armor = 100,
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 5,
+	light_damage = 2,
+	on_rightclick = nil,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 15,
+		speed_run = 15,
+		stand_start = 0,
+		stand_end = 14,
+		walk_start = 15,
+		walk_end = 38,
+		run_start = 40,
+		run_end = 63,
+		punch_start = 40,
+		punch_end = 63,
+	},
+	jump = true,
+	sounds = {
+		attack = "mob_attack",
+		death = "mob_death",
+	},
+	step = 0.5,
+})
+
+mobs:register_mob("mobs:dirt_monster2", {
+	type = "monster",
+	hp_min = 3,
+	hp_max = 27,
+	exp_min = 0,
+	exp_max = 35,
+	collisionbox = {-0.4, -0.01, -0.4, 0.4, 1.9, 0.4},
+	visual = "mesh",
+	mesh = "mobs_stone_monster.x",
+	textures = {"mobs_dirt_monster.png"},
+	visual_size = {x=3, y=2.6},
+	makes_footstep_sound = true,
+	view_range = 15,
+	walk_velocity = 1.25,
+	run_velocity = 3.75,
+	damage = 2,
+	drops = {
+		{name = "default:dirt",
+		chance = 1,
+		min = 3,
+		max = 5,},
+	},
+	armor = 100,
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 5,
+	light_damage = 0,
+	on_rightclick = nil,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 15,
+		speed_run = 15,
+		stand_start = 0,
+		stand_end = 14,
+		walk_start = 15,
+		walk_end = 38,
+		run_start = 40,
+		run_end = 63,
+		punch_start = 40,
+		punch_end = 63,
+	},
+	jump = true,
+	sounds = {
+		attack = "mob_attack",
+		death = "mob_death1",
+	},
+	step = 0.5,
+})
+
+mobs:register_spawn("mobs:dirt_monster", {"default:dirt_with_grass"}, 3, -1, 5000, 3, 31000)
+mobs:register_spawn("mobs:dirt_monster2", {"mg:dirt_with_dry_grass"}, 20, -1, 3000, 4, 31000)
+
+mobs:register_mob("mobs:snow_monster", {
+	type = "monster",
+	hp_min = 10,
+	hp_max = 35,
+	exp_min = 3,
+	exp_max = 35,
+	collisionbox = {-0.35,-1.0,-0.35, 0.35,0.8,0.35},
+	visual = "mesh",
+	mesh = "character.x",
+	textures = {"mobs_snow_monster.png"},
+	visual_size = {x=1, y=1},
+	makes_footstep_sound = true,
+	view_range = 15,
+	walk_velocity = 1,
+	run_velocity = 3,
+	damage = 2,
+	drops = { },
+	armor = 100,
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 5,
+	light_damage = 2,
+	on_rightclick = nil,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 30,
+		speed_run = 30,
+		stand_start = 0,
+		stand_end = 79,
+		walk_start = 168,
+		walk_end = 187,
+		run_start = 168,
+		run_end = 187,
+		punch_start = 200,
+		punch_end = 219,
+	},
+	jump = true,
+	sounds = {
+		death = "mobs_yeti_death",
+		random = "mobs_howl",
+		attack = "mobs_slash_attack",
+	},
+	step = 0.5,
+})
+mobs:register_spawn("mobs:snow_monster", {"default:snow","default:snow_with_grass"}, 10, -1, 7000, 2, 31000)
+
+
+mobs:register_mob("mobs:stone_monster", {
+	type = "monster",
+	hp_min = 12,
+	hp_max = 35,
+	exp_min = 3,
+	exp_max = 55,
+	collisionbox = {-0.4, -0.01, -0.4, 0.4, 1.9, 0.4},
+	visual = "mesh",
+	mesh = "mobs_stone_monster.x",
+	textures = {"mobs_stone_monster.png"},
+	visual_size = {x=3, y=2.6},
+	makes_footstep_sound = true,
+	view_range = 10,
+	walk_velocity = 0.5,
+	run_velocity = 2,
+	damage = 3,
+	drops = {
+		{name = "default:torch",
+		chance = 2,
+		min = 3,
+		max = 5,},
+		{name = "default:iron_lump",
+		chance=5,
+		min=1,
+		max=2,},
+		{name = "default:coal_lump",
+		chance=3,
+		min=1,
+		max=3,},
+	},
+	light_resistant = true,
+	armor = 80,
+	drawtype = "front",
+	water_damage = 0,
+	lava_damage = 0,
+	light_damage = 0,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 15,
+		speed_run = 15,
+		stand_start = 0,
+		stand_end = 14,
+		walk_start = 15,
+		walk_end = 38,
+		run_start = 40,
+		run_end = 63,
+		punch_start = 40,
+		punch_end = 63,
+	},
+	sounds = {
+		attack = "mobs_stone_attack",
+		death = "mobs_stone_death",
+		random = "mobs_stone",
+	},
+	step = 0.5,
+})
+mobs:register_spawn("mobs:stone_monster", {"default:stone"}, 3, -1, 3000, 3, 0)
+
+mobs:register_mob("mobs:sand_monster", {
+	type = "monster",
+	hp_min = 4,
+	hp_max = 20,
+	exp_min = 1,
+	exp_max = 65,
+	collisionbox = {-0.4, -0.01, -0.4, 0.4, 1.9, 0.4},
+	visual = "mesh",
+	mesh = "mobs_sand_monster.x",
+	textures = {"mobs_sand_monster.png"},
+	visual_size = {x=8,y=8},
+	makes_footstep_sound = true,
+	view_range = 15,
+	walk_velocity = 1.5,
+	run_velocity = 4.5,
+	damage = 1,
+	drops = {
+	},
+	light_resistant = true,
+	armor = 100,
+	drawtype = "front",
+	water_damage = 3,
+	lava_damage = 1,
+	light_damage = 0,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 15,
+		speed_run = 15,
+		stand_start = 0,
+		stand_end = 39,
+		walk_start = 41,
+		walk_end = 72,
+		run_start = 74,
+		run_end = 105,
+		punch_start = 74,
+		punch_end = 105,
+	},
+	jump = true,
+	sounds = {
+		attack = "mob_attack",
+		death = "mob_death2",
+	},
+	step = 0.5,
+})
+mobs:register_spawn("mobs:sand_monster", {"default:sand"}, 20, -1, 8000, 4, 31000)
+mobs:register_spawn("mobs:sand_monster", {"default:desert_sand"}, 20, -1, 4000, 4, 31000)
+
+mobs:register_mob("mobs:tree_monster", {
+	type = "monster",
+	hp_min = 7,
+	hp_max = 33,
+	exp_min=2,
+	exp_max=75,
+	collisionbox = {-0.4, -0.01, -0.4, 0.4, 1.9, 0.4},
+	visual = "mesh",
+	mesh = "mobs_tree_monster.x",
+	textures = {"mobs_tree_monster.png"},
+	visual_size = {x=4.5,y=4.5},
+	makes_footstep_sound = true,
+	view_range = 15,
+	walk_velocity = 1,
+	run_velocity = 3,
+	damage = 2,
+	drops = {
+		{name = "default:sapling",
+		chance = 3,
+		min = 1,
+		max = 2,},
+		{name = "default:junglesapling",
+		chance = 3,
+		min = 1,
+		max = 2,},
+		{name = "default:apple",
+			chance = 2,
+			min = 1,
+			max=3,
+		},
+	},
+	light_resistant = true,
+	armor = 100,
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 5,
+	light_damage = 2,
+	disable_fall_damage = true,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 15,
+		speed_run = 15,
+		stand_start = 0,
+		stand_end = 24,
+		walk_start = 25,
+		walk_end = 47,
+		run_start = 48,
+		run_end = 62,
+		punch_start = 48,
+		punch_end = 62,
+	},
+	jump = true,
+	sounds = {
+		attack = "mob_attack",
+		death = "mob_death1",
+	},
+	step = 0.5,
+})
+mobs:register_spawn("mobs:tree_monster", {"default:leaves", "default:jungleleaves"}, 3, -1, 7000, 3, 31000)
+
+mobs:register_mob("mobs:sheep", {
+	type = "animal",
+	hp_min = 8,
+	hp_max = 10,
+	collisionbox = {-0.4, -0.01, -0.4, 0.4, 1, 0.4},
+	textures = {"mobs_sheep.png"},
+	visual = "mesh",
+	mesh = "mobs_sheep.x",
+	makes_footstep_sound = true,
+	walk_velocity = 1,
+	armor = 200,
+	drops = {
+		{name = "mobs:meat_raw",
+		chance = 1,
+		min = 2,
+		max = 3,},
+	},
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 5,
+	light_damage = 0,
+	sounds = {
+		random = "mobs_sheep",
+	},
+	animation = {
+		speed_normal = 15,
+		stand_start = 0,
+		stand_end = 80,
+		walk_start = 81,
+		walk_end = 100,
+	},
+	follow = "farming:wheat",
+	view_range = 5,
+	
+	on_rightclick = function(self, clicker)
+		local item = clicker:get_wielded_item()
+		if item:get_name() == "farming:wheat" then
+			if not self.tamed then
+				if not minetest.setting_getbool("creative_mode") then
+					item:take_item()
+					clicker:set_wielded_item(item)
+				end
+				self.tamed = true
+			elseif self.naked then
+				if not minetest.setting_getbool("creative_mode") then
+					item:take_item()
+					clicker:set_wielded_item(item)
+				end
+				self.food = (self.food or 0) + 1
+				if self.food >= 8 then
+					self.food = 0
+					self.naked = false
+					self.object:set_properties({
+						textures = {"mobs_sheep.png"},
+						mesh = "mobs_sheep.x",
+					})
+				end
+			end
+			return
+		end
+		if clicker:get_inventory() and not self.naked then
+			self.naked = true
+			if minetest.registered_items["wool:white"] then
+				clicker:get_inventory():add_item("main", ItemStack("wool:white "..math.random(1,3)))
+			end
+			self.object:set_properties({
+				textures = {"mobs_sheep_shaved.png"},
+				mesh = "mobs_sheep_shaved.x",
+			})
+		end
+	end,
+	jump = true,
+	step=1,
+})
+mobs:register_spawn("mobs:sheep", {"default:dirt_with_grass"}, 20, 8, 9000, 2, 31000)
+
+minetest.register_craftitem("mobs:meat_raw", {
+	description = "Raw Meat",
+	inventory_image = "mobs_meat_raw.png",
+	on_use = function(itemstack, user, pointed_thing)
+		affects.affectPlayer(user:get_player_name(),"food_poisoning")
+		itemstack:take_item()
+		return itemstack
+	end, 
+})
+
+minetest.register_craftitem("mobs:meat", {
+	description = "Meat",
+	inventory_image = "mobs_meat.png",
+	on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "mobs:meat",
+	recipe = "mobs:meat_raw",
+	cooktime = 5,
+})
+
+mobs:register_mob("mobs:rat", {
+	type = "animal",
+	hp_min = 1,
+	hp_max = 4,
+	collisionbox = {-0.2, -0.01, -0.2, 0.2, 0.2, 0.2},
+	visual = "mesh",
+	mesh = "mobs_rat.x",
+	textures = {"mobs_rat.png"},
+	makes_footstep_sound = false,
+	walk_velocity = 1,
+	armor = 200,
+	drops = {},
+	drawtype = "front",
+	water_damage = 0,
+	lava_damage = 1,
+	light_damage = 0,
+	
+	on_rightclick = function(self, clicker)
+		if clicker:is_player() and clicker:get_inventory() then
+			clicker:get_inventory():add_item("main", "mobs:rat")
+			self.object:remove()
+		end
+	end,
+	sounds = { },
+	step=1,
+})
+mobs:register_spawn("mobs:rat", {"default:dirt_with_grass", "default:stone"}, 20, -1, 7000, 1, 31000)
+
+minetest.register_craftitem("mobs:rat", {
+	description = "Rat",
+	inventory_image = "mobs_rat_inventory.png",
+	
+	on_place = function(itemstack, placer, pointed_thing)
+		if pointed_thing.above then
+			minetest.env:add_entity(pointed_thing.above, "mobs:rat")
+			itemstack:take_item()
+		end
+		return itemstack
+	end,
+})
+	
+minetest.register_craftitem("mobs:rat_cooked", {
+	description = "Cooked Rat",
+	inventory_image = "mobs_cooked_rat.png",
+	
+	on_use = minetest.item_eat(3),
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "mobs:rat_cooked",
+	recipe = "mobs:rat",
+	cooktime = 5,
+})
+
+mobs:register_mob("mobs:oerkki", {
+	type = "monster",
+	hp_min = 8,
+	hp_max = 34,
+	exp_min=1,
+	exp_max=88,
+	collisionbox = {-0.4, -0.01, -0.4, 0.4, 1.9, 0.4},
+	visual = "mesh",
+	mesh = "mobs_oerkki.x",
+	textures = {"mobs_oerkki.png"},
+	visual_size = {x=5, y=5},
+	makes_footstep_sound = false,
+	view_range = 15,
+	walk_velocity = 1,
+	run_velocity = 3,
+	damage = 4,
+	drops = {},
+	armor = 100,
+	drawtype = "front",
+	light_resistant = true,
+	water_damage = 1,
+	lava_damage = 1,
+	light_damage = 0,
+	attack_type = "dogfight",
+	animation = {
+		stand_start = 0,
+		stand_end = 23,
+		walk_start = 24,
+		walk_end = 36,
+		run_start = 37,
+		run_end = 49,
+		punch_start = 37,
+		punch_end = 49,
+		speed_normal = 15,
+		speed_run = 15,
+	},
+	jump = true,
+	sounds = { 
+		random = "mobs_eerie",
+		attack = "mobs_oerkki_attack"
+	},
+	step=0.5,
+	
+})
+mobs:register_spawn("mobs:oerkki", {"default:stone"}, 3, -1, 7000, 3, -10)
+mobs:register_spawn("mobs:oerkki", {"default:stone"}, 3, -1, 2000, 3, -100)
+
+mobs:register_mob("mobs:dungeon_master", {
+	type = "monster",
+	hp_min = 12,
+	hp_max = 47,
+	exp_min=5,
+	exp_max=100,
+	collisionbox = {-0.7, -0.01, -0.7, 0.7, 2.6, 0.7},
+	visual = "mesh",
+	mesh = "mobs_dungeon_master.x",
+	textures = {"mobs_dungeon_master.png"},
+	visual_size = {x=8, y=8},
+	makes_footstep_sound = true,
+	view_range = 15,
+	walk_velocity = 1,
+	run_velocity = 3,
+	damage = 4,
+	drops = {
+		{name = "default:mese",
+		chance = 100,
+		min = 1,
+		max = 2,},
+	},
+	armor = 60,
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 1,
+	light_damage = 0,
+	on_rightclick = nil,
+	attack_type = "shoot",
+	arrow = "mobs:fireball",
+	shoot_interval = 2.5,
+	sounds = {
+		attack = "mobs_fireball",
+	},
+	animation = {
+		stand_start = 0,
+		stand_end = 19,
+		walk_start = 20,
+		walk_end = 35,
+		punch_start = 36,
+		punch_end = 48,
+		speed_normal = 15,
+		speed_run = 15,
+	},
+	jump = true,
+	step=0.5,
+})
+mobs:register_spawn("mobs:dungeon_master", {"default:stone"}, 3, -1, 7000, 1, -50)
+
+mobs:register_arrow("mobs:fireball", {
+	visual = "sprite",
+	visual_size = {x=1, y=1},
+	--textures = {{name="mobs_fireball.png", animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=0.5}}}, FIXME
+	textures = {"mobs_fireball.png"},
+	velocity = 5,
+	hit_player = function(self, player)
+		local s = self.object:getpos()
+		local p = player:getpos()
+		local vec = {x=s.x-p.x, y=s.y-p.y, z=s.z-p.z}
+		player:punch(self.object, 1.0,  {
+			full_punch_interval=1.0,
+			damage_groups = {fleshy=4},
+		}, vec)
+		
+		local snum = math.random(1,4)
+		minetest.sound_play("default_hurt"..tostring(snum),{
+			pos = p,
+			max_hear_distance = 50,
+			gain = 10,
+		})
+		
+		local pos = self.object:getpos()
+		for dx=-1,1 do
+			for dy=-1,1 do
+				for dz=-1,1 do
+					local p = {x=pos.x+dx, y=pos.y+dy, z=pos.z+dz}
+					local n = minetest.env:get_node(pos).name
+					if minetest.registered_nodes[n].groups.flammable or math.random(1, 100) <= 30 then
+						minetest.env:set_node(p, {name="fire:basic_flame"})
+					else
+						minetest.env:remove_node(p)
+					end
+				end
+			end
+		end
+	end,
+	hit_node = function(self, pos, node)
+		for dx=-1,1 do
+			for dy=-2,1 do
+				for dz=-1,1 do
+					local p = {x=pos.x+dx, y=pos.y+dy, z=pos.z+dz}
+					local n = minetest.env:get_node(pos).name
+					if minetest.registered_nodes[n].groups.flammable or math.random(1, 100) <= 30 then
+						minetest.env:set_node(p, {name="fire:basic_flame"})
+					else
+						minetest.env:remove_node(p)
+					end
+				end
+			end
+		end
+	end
+})
+
+mobs:register_mob("mobs:barbarian1", {
+	type = "monster",
+	hp_min = 20,
+	hp_max = 65,
+	exp_min = 10,
+	exp_max = 65,
+	collisionbox = {-0.35,-1.0,-0.35, 0.35,0.8,0.35},
+	visual = "mesh",
+	mesh = "3d_armor_character.x",
+	textures = {"mobs_barbarian1.png",	
+				"3d_armor_trans.png",
+				"3d_armor_trans.png",
+			},
+	visual_size = {x=1, y=1},
+	makes_footstep_sound = true,
+	view_range = 10,
+	walk_velocity = 2,
+	run_velocity = 4,
+	damage = 4,
+	drops = { },
+	armor = 150,
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 5,
+	light_damage = 0,
+	on_rightclick = nil,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 30,
+		speed_run = 30,
+		stand_start = 0,
+		stand_end = 79,
+		walk_start = 168,
+		walk_end = 187,
+		run_start = 168,
+		run_end = 187,
+		punch_start = 200,
+		punch_end = 219,
+	},
+	jump = true,
+	sounds = {
+		war_cry = "mobs_barbarian_yell2",
+		death = "mobs_barbarian_death",
+		attack = "default_punch2",
+	},
+	step=0.5,
+})
+
+mobs:register_mob("mobs:barbarian2", {
+	type = "monster",
+	hp_min = 20,
+	hp_max = 65,
+	exp_min = 10,
+	exp_max = 65,
+	collisionbox = {-0.35,-1.0,-0.35, 0.35,0.8,0.35},
+	visual = "mesh",
+	mesh = "3d_armor_character.x",
+	textures = {"mobs_barbarian2.png",	
+				"3d_armor_trans.png",
+				"3d_armor_trans.png",
+			},
+	visual_size = {x=1, y=1},
+	makes_footstep_sound = true,
+	view_range = 10,
+	walk_velocity = 2,
+	run_velocity = 4,
+	damage = 4,
+	drops = { },
+	armor = 150,
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 5,
+	light_damage = 0,
+	on_rightclick = nil,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 30,
+		speed_run = 30,
+		stand_start = 0,
+		stand_end = 79,
+		walk_start = 168,
+		walk_end = 187,
+		run_start = 168,
+		run_end = 187,
+		punch_start = 200,
+		punch_end = 219,
+	},
+	jump = true,
+	sounds = {
+		war_cry = "mobs_barbarian_yell1",
+		death = "mobs_barbarian_death",
+		attack = "default_punch2",
+	},
+	step=0.5,
+})
+
+local female_npc_base = {
+type = "npc",
+	hp_min = 30,
+	hp_max = 75,
+	exp_min = 0,
+	exp_max = 0,
+	collisionbox = {-0.35,-1.0,-0.35, 0.35,0.8,0.35},
+	visual = "mesh",
+	mesh = "3d_armor_character.x",
+	textures = {"mobs_female1.png",	
+				"3d_armor_trans.png",
+				"3d_armor_trans.png",
+			},
+	visual_size = {x=1, y=1},
+	makes_footstep_sound = true,
+	view_range = 12,
+	walk_velocity = 1.25,
+	run_velocity = 3.75,
+	damage = 6,
+	drops = { },
+	armor = 150,
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 5,
+	light_damage = 0,
+	on_rightclick = nil,
+	walk_chance = 1,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 30,
+		speed_run = 30,
+		stand_start = 0,
+		stand_end = 79,
+		walk_start = 168,
+		walk_end = 187,
+		run_start = 168,
+		run_end = 187,
+		punch_start = 200,
+		punch_end = 219,
+	},
+	jump = false,
+	sounds = {
+		attack = "default_punch",
+	},
+	attacks_monsters=false,
+	peaceful = true,
+	step=2,
+}
+
+local female2 = female_npc_base
+female2.textures = {"mobs_female2.png", "3d_armor_trans.png","3d_armor_trans.png"}
+
+local female3 = female_npc_base
+female3.textures = {"mobs_female3.png", "3d_armor_trans.png","3d_armor_trans.png"}
+
+mobs:register_mob("mobs:female1_npc", female_npc_base )
+mobs:register_mob("mobs:female2_npc", female2 )
+mobs:register_mob("mobs:female3_npc", female3 )
+
+local male_npc_base = {
+	type = "npc",
+	hp_min = 30,
+	hp_max = 75,
+	exp_min = 0,
+	exp_max = 0,
+	collisionbox = {-0.35,-1.0,-0.35, 0.35,0.8,0.35},
+	visual = "mesh",
+	mesh = "3d_armor_character.x",
+	textures = {"mobs_male1.png",	
+				"3d_armor_trans.png",
+				"3d_armor_trans.png",
+			},
+	visual_size = {x=1, y=1},
+	makes_footstep_sound = true,
+	view_range = 12,
+	walk_velocity = 1.25,
+	run_velocity = 4.5,
+	damage = 6,
+	drops = { },
+	armor = 150,
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 5,
+	light_damage = 0,
+	on_rightclick = nil,
+	walk_chance = 1,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 30,
+		speed_run = 30,
+		stand_start = 0,
+		stand_end = 79,
+		walk_start = 168,
+		walk_end = 187,
+		run_start = 168,
+		run_end = 187,
+		punch_start = 200,
+		punch_end = 219,
+	},
+	jump = false,
+	sounds = {
+		war_cry = "mobs_die_yell",
+		death = "default_death",
+		attack = "default_punch2",
+	},
+	attacks_monsters=true,
+	peaceful = true,
+	group_attack = true,
+	step=2,
+}
+
+local male2 = female_npc_base
+male2.textures = {"mobs_male2.png", "3d_armor_trans.png","3d_armor_trans.png"}
+
+local male3 = female_npc_base
+male3.textures = {"mobs_male3.png", "3d_armor_trans.png","3d_armor_trans.png"}
+
+
+mobs:register_mob("mobs:male1_npc", male_npc_base)
+mobs:register_mob("mobs:male2_npc", male2)
+mobs:register_mob("mobs:male3_npc", male3)
+
+if minetest.setting_get("log_mods") then
+	minetest.log("action", "mobs loaded")
+end
